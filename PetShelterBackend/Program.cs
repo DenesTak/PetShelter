9ca38dda29afb6d732db29c4d5e88556b139e696
Denes TAKACS <tak20432@spengergasse.at>
2023-12-10 12:20:35 +0100

    chore(backend): add swagger

---
+++ PetShelterBackend/Program.cs
@@ -0,0 +1,14 @@
+using PetShelterBackend;
+
+var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+await startup.Configure(app, builder.Environment);
+
+app.Run();
---
+++ PetShelterBackend/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace PetShelterBackend;
+
+public class Startup
+{
+    public Startup(ConfigurationManager builderConfiguration)
+    {
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "PetShelter API",
+                Version = "v1",
+                Description = "API for managing pets in a shelter"
+            });
+        });
+        
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public async Task Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PetShelter API V1");
+                c.RoutePrefix = "swagger";
+            });
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+}
